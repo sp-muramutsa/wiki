e5d5e15d3896f6d8a9805bcdf91283294451fb19
sp-muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-04-12 10:51:02 -0500

    Clicking “Create New Page” in the sidebar now takes the user to a page where they can create a new encyclopedia entry.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>/", views.entry, name="entry"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("new/", views.new, name="new")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect, render, reverse
 
 from . import util
 
+from .forms import NewEntryForm
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -29,7 +31,6 @@ def search(request):
         matches = [entry for entry in entries if query.lower() in entry.lower()]
         exact_match = [entry for entry in entries if query.lower() == entry.lower()]
 
-        print(matches)
         if exact_match:
             return HttpResponseRedirect(reverse("entry", kwargs={"title": exact_match}))
         
@@ -40,4 +41,32 @@ def search(request):
             })
     else:
         return redirect(reverse("index"))
+
+def new(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            if util.get_entry(title):
+                return render(request, "encyclopedia/error.html", {
+                    "message": "Entry Already Exosts in Wiki",
+                    "title": title
+                })
+            else:
+                util.save_entry(title, content)
+                return HttpResponseRedirect(reverse("encyclopedia:entry", kwargs={"title":title}))
+        else:
+            pass
+    else:
+        form = NewEntryForm()
+        return render(request, "encyclopedia/new.html", {
+            "form": form
+        })
+
+        
+        
+        
+        
             
