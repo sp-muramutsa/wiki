d142259a601d2e9bc96ff55c3966b45a7b4360c3
sp-muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-04-11 19:21:27 -0500

    Implemented the entry view which renders a page that displays the contents of an encyclopedia entry with title TITLE whenever user visits /wiki/TITLE

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry = util.get_entry(title)
+    if entry:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": entry
+        })
+
+    else: 
+        return render(request, "encyclopedia/404.html", {
+            "title": title
+        })
+    
